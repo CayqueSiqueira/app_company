697f3e0037304a9494c2729edf6b21291568ad69
Cayque Siqueira <96526342+CayqueSiqueira@users.noreply.github.com>
2023-09-11 20:26:45 -0300

    Add files via upload

---
+++ app_company/src/server.ts
@@ -0,0 +1,28 @@
+import fastify from 'fastify';
+
+import { PrismaClient } from '@prisma/client';
+
+const app = fastify()
+
+const prisma = new PrismaClient();
+
+app.get('/groups', async () => {
+    const group = await prisma.group.findMany()
+    return group
+})
+
+app.get('/storages', async () => {
+    const storage = await prisma.storage.findMany()
+    return storage
+})
+
+app.get('/products', async () => {
+    const products = await prisma.products.findMany()
+    return products
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('Server is running on port 3333')
+})
